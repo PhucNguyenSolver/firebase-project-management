9b059e0b9966363bd66edcb953a15fd8afb60a19
PhucNguyenSolver <phuc.nguyensolver@hcmut.edu.vn>
2023-07-11 18:28:39 +0700

    refactor login: merge file

--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -1,8 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { onAuthStateChanged, getAuth } from 'firebase/auth';
-import { loginWith, loginGuest } from './AuthService'
 import { Spin } from 'antd';
 import { phuc } from './user-data.js'
+import { serverTimestamp } from 'firebase/firestore';
+import { onAuthStateChanged, signInWithPopup, GoogleAuthProvider, getAuth, FacebookAuthProvider, getAdditionalUserInfo, signInAnonymously } from "firebase/auth";
+import { addDocument, generateKeywords } from '../firebase/service';
+
+let getAuthProvider = (function () {
+  const fbInstance = new FacebookAuthProvider()
+  const ggInstance = new GoogleAuthProvider()
+  return function (str) {
+    switch (str) {
+      case "google": return ggInstance
+      case "facebook": return fbInstance
+      default: throw "Not implemented"
+    }
+  }
+})()
+
+/**
+ * 
+ * @param {string} provider google / facebook / ...
+ * Throw if not implemented
+ */
+async function loginWith(provider) {
+  let data = await signInWithPopup(getAuth(), getAuthProvider(provider))
+  if (getAdditionalUserInfo(data).isNewUser) {
+    // TODO: (refactor) firebase service should not be here
+    const userInfo = {
+      name: data.user.displayName,
+      email: data.user.email,
+      uid: data.user.uid,
+      avaURL: data.user.photoURL,
+      createdAt: serverTimestamp(),
+      keywords: generateKeywords(data.user.displayName?.toLowerCase()),
+    }
+    await addDocument("person", userInfo)
+  }
+}
+
+async function loginGuest() {
+  let auth = getAuth();
+  let credential = await signInAnonymously(auth)
+  console.log({ credential })
+}
+
 
 export const AuthContext = React.createContext();
 
