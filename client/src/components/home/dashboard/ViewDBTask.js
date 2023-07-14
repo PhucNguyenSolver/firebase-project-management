116494c7631ad058f9634c658606a80dbeef59fa
nguyenhuuphuc.cse <nguyenhuuphuc.cse@gmail.com>
2023-07-14 16:07:04 +0700

    refactor: make use of ModalViewTask

--- client/src/components/home/dashboard/ViewDBTask.js
+++ client/src/components/home/dashboard/ViewDBTask.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Input, Col, Row, Modal } from 'antd';
-import { Tag } from 'antd';
-import './task.scss';
 import { AppContext } from '../../../context/AppProvider';
+import ModalViewTask from '../../modal/ModalViewTask';
 
 // Data got from DB
 
@@ -19,120 +17,29 @@ import { AppContext } from '../../../context/AppProvider';
 //     desc: "Sample Text Sample Text Sample Text Sample Text Sample Text"
 // }
 
-const { TextArea } = Input;
 
-export default function ViewDBTask() {
-    const { visibleDBTask, setVisibleDBTask, curDBTask, DBmemberList } = React.useContext(AppContext);
-    const title = curDBTask.name;
-
-    const createDate = curDBTask.createDate;
-
-    const dl = curDBTask.deadline;
-
-    const priority = curDBTask.priority;
-
-    const tags = curDBTask.tag;
-
-    const AA = curDBTask.memberIdList;
-
-    const prog = curDBTask.progression;
-
-    const desc = curDBTask.description;
-
+export default function ModalViewTaskFromDashboard() {
+    const { visibleDBTask, setVisibleDBTask, curDBTask: task, DBmemberList } = React.useContext(AppContext);
 
     function convertIDtoName(uid) {
         return DBmemberList.find(o => o.uid === uid)?.name;
     }
 
-    function closeModal() {
-        setVisibleDBTask(false);
-    }
+    const assignees = task.memberIdList.map(memId => convertIDtoName(memId))
 
-    return (
-        <Modal className="TaskModal" visible={visibleDBTask} width={700} onCancel={closeModal} onOk={closeModal} footer={null}>
-            {/* Title */}
-            <Row className="title-row">
-                <Col span={15}>
-                    <Input value={title} placeholder="Title" size="large" bordered={false} readOnly={true} />
-                </Col>
-            </Row>
-
-            {/* Date & Deadline */}
-            <Row className="normal-row">
-                <Col span={5} className="element-text">Create Date:</Col>
-                <Col span={7}>
-                    <input type="text" size={20} value={createDate} readOnly />
-                </Col>
-                <Col span={5} className="element-text align-pair">Due Date:</Col>
-                <Col span={5}>
-                    <input type="text" size={15} value={dl === "" ? "None" : dl} readOnly />
-                </Col>
-            </Row>
-
-            {/* Creator & Priority */}
-            <Row className="normal-row">
-                <Col span={5} className='element-text'>Priority:</Col>
-                <Col span={5}>
-                    <input type="text" size={10} value={priority} readOnly />
-                </Col>
-            </Row>
-
-            {/* Tag */}
-            <Row className="normal-row">
-                <Col span={5} className="element-text">Tag:</Col>
-                {tags?.map((tag) => {
-                    const isLongTag = tag.length > 7;
-                    const tagElem = (
-                        <Tag
-                            key={tag}
-                            closable={false}
-                        >
-                            <span style={{ fontSize: '14px' }}> {isLongTag ? `${tag.slice(0, 7)}...` : tag} </span>
-                        </Tag>
-                    );
-                    return tagElem;
-                })}
-            </Row>
-
-
-            {/* Assigns */}
-            <Row className="normal-row">
-                <Col span={5} className="element-text">Assign to:</Col>
-            </Row>
-
-            <Row className="normal-row">
-                <div>
-                    {AA.map((memberID) => {
-                        const memElem = (
-                            <Tag
-                                key={memberID}
-                                closable={false}
-                            >
-                                {convertIDtoName(memberID)}
-                            </Tag>
-                        );
-                        return memElem;
-                    })}
-                </div>
-            </Row>
-
-            {/* Progression */}
-            <Row className="normal-row">
-                <Col span={5} className="element-text">Progression:</Col>
-                <Col span={4}>
-                    <input type="text" size={10} value={prog + " %"} readOnly />
-                </Col>
-            </Row>
-
-            {/* Description */}
-            <Row className="normal-row">
-                <h1 className="element-text">Description:</h1>
-            </Row>
-
-            {/* DescTextArea */}
-            <Row className="desc-row">
-                <TextArea readOnly={true} value={desc} placeholder="Description" autoSize={{ minRows: 5, maxRows: 10 }} />
-            </Row>
-        </Modal>
-    )
+    return <ModalViewTask
+        visible={visibleDBTask}
+        onBlur={() => setVisibleDBTask(false)}
+        data={{
+            title: task.name,
+            createDate: task.createDate,
+            dl: task.deadline,
+            priority: task.priority,
+            tags: task.tag,
+            AA: task.memberIdList,
+            prog: task.progression,
+            desc: task.description,
+            assignees: assignees,
+        }}
+    />
 }
